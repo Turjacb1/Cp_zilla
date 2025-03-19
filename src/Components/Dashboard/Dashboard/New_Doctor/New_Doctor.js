5a3f1878945eb2972a5e9eee347f0ba3a49f222a
Turja Chakraborty <152412436+Turjacb1@users.noreply.github.com>
2025-03-19 16:43:58 +0600

    render

--- src/Components/Dashboard/Dashboard/New_Doctor/New_Doctor.js
+++ src/Components/Dashboard/Dashboard/New_Doctor/New_Doctor.js
@@ -87,6 +87,7 @@
 
 
 
+
 import React, { useState } from 'react';
 import './New_Doctor.css';
 
@@ -108,7 +109,7 @@ const New_Doctor = () => {
         
         // Send data to the backend using fetch
         try {
-            const response = await fetch('http://localhost:5000/api/doctors', {
+            const response = await fetch('https://cp-zilla.onrender.com/api/doctors', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
